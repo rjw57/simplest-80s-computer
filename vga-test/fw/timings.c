8e4944d878fea1ffe92dff9147d6913002f270f4
Rich Wareham <rich@richwareham.com>
2018-06-04 22:12:49 +0100

    update VGA to latest version

--- vga-test/fw/timings.c
+++ vga-test/fw/timings.c
@@ -1,3 +1,6 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 // VGA timing generator.
 //
 // Output HSYNC, VSYNC, HVIS and VVIS signals.
@@ -6,25 +9,32 @@
 // and need to be synchronised with the HSYNC signal by means of an external
 // D-type flip-flop.
 
-// Pin assignment - Arduino Nano
-const int HSYNC_PIN   = 5;    // =OC0B, fixed by hardware
-const int HVIS_PIN    = 3;    // =OC2B, fixed by hardware
-const int VSYNC_PIN   = 4;
-const int VVIS_PIN    = 2;
+// Pin assignment - ATMEGA328P
+#define HSYNC_PORT_BIT  5   // =OC0B, pin 11, fixed by hardware
+#define HSYNC_PORT_NAME D
+#define HVIS_PORT_BIT   3   // =OC2B, pin 5, fixed by hardware
+#define HVIS_PORT_NAME  D
+#define VSYNC_PORT_BIT  1   // pin 3, free choice
+#define VSYNC_PORT_NAME D
+#define VVIS_PORT_BIT   2   // pin 4, free choice
+#define VVIS_PORT_NAME  D
 
-#if 0
-// 640x480 VESA with slightly wrong dot clock
-const double dot_clock_freq       = 32;               // MHz
+#define HEARTBEAT_PORT_BIT  0   // pin 2, free choice
+#define HEARTBEAT_PORT_NAME D
+
+#if 1
+// 640x480 VGA @ 60Hz
+const double dot_clock_freq       = 25.175;           // MHz
 const int h_visible_area          = 640;              // pixels
 const int h_front_porch           = 16;               // pixels
-const int h_sync_width            = 64;               // pixels
-const int h_back_porch            = 120;              // pixels
+const int h_sync_width            = 96;               // pixels
+const int h_back_porch            = 48;               // pixels
 const int h_polarity              = -1;               // sign
 
 const int v_visible_area          = 480;              // lines
-const int v_front_porch           = 1;                // lines
-const int v_sync_width            = 3;                // lines
-const int v_back_porch            = 16;               // lines
+const int v_front_porch           = 10;               // lines
+const int v_sync_width            = 2;                // lines
+const int v_back_porch            = 33;               // lines
 const int v_polarity              = -1;               // sign
 #endif
 
@@ -44,8 +54,8 @@ const int v_back_porch            = 23;               // lines
 const int v_polarity              = +1;               // sign
 #endif
 
-#if 1
-// 1024x768 XGA with slightly odd dot clock
+#if 0
+// 1024x768 XGA
 const double dot_clock_freq       = 64;               // MHz
 const int h_visible_area          = 1024;             // pixels
 const int h_front_porch           = 24;               // pixels
@@ -76,40 +86,69 @@ const int whole_frame             =
     v_visible_area + v_front_porch + v_sync_width + v_back_porch;
 
 // Counter timer
-const double timer_freq           = 16;               // MHz
+const double timer_freq           = dot_clock_freq / 2;               // MHz
 
-// Called around the middle of the HVIS inactive pulse. The delay between the
-// interrupt flag being set and the pin actually changing is slightly
-// non-deterministic since the current instruction needs to finish processing.
-// We intentionally allow for slop by outputting signals for the *next* line and
-// requiring that the signals be synchronised with the following HSYNC pulse.
+// CPU frequency is the same as the timer frequency.
+#define F_CPU (unsigned long)(timer_freq * 1e6)
+
+// Not F_CPU is set, we can include the delay utilities.
+#include <util/delay.h>
+
+#define PASTER(x,y) x ## y
+#define EVALUATOR(x, y) PASTER(x, y)
+
+#define set_pin_output(port_name, port_bit) do { \
+  EVALUATOR(DDR, port_name) |= _BV(EVALUATOR(PORT, EVALUATOR(port_name, port_bit))); \
+} while(0)
+
+#define set_pin(port_name, port_bit) do { \
+  EVALUATOR(PORT, port_name) |= _BV(EVALUATOR(PORT, EVALUATOR(port_name, port_bit))); \
+} while(0)
+
+#define reset_pin(port_name, port_bit) do { \
+  EVALUATOR(PORT, port_name) &= ~_BV(EVALUATOR(PORT, EVALUATOR(port_name, port_bit))); \
+} while(0)
+
+// Called around the middle of the HSYNC pulse. The delay between the interrupt
+// flag being set and the pin actually changing is slightly non-deterministic
+// since the current instruction needs to finish processing. We allow for slop
+// by outputting signals for the *next* line and requiring that the signals be
+// synchronised with the following HVIS pulse.
 static int row_counter = 0;
-ISR(TIMER2_OVF_vect) {
+ISR(TIMER0_OVF_vect) {
   ++row_counter;
 
   switch(row_counter) {
     case v_front_porch:
-      digitalWrite(VSYNC_PIN, (v_polarity < 0) ? LOW : HIGH);
+      if(v_polarity < 0) {
+        reset_pin(VSYNC_PORT_NAME, VSYNC_PORT_BIT);
+      } else {
+        set_pin(VSYNC_PORT_NAME, VSYNC_PORT_BIT);
+      }
       break;
     case v_front_porch + v_sync_width:
-      digitalWrite(VSYNC_PIN, (v_polarity < 0) ? HIGH : LOW);
+      if(v_polarity < 0) {
+        set_pin(VSYNC_PORT_NAME, VSYNC_PORT_BIT);
+      } else {
+        reset_pin(VSYNC_PORT_NAME, VSYNC_PORT_BIT);
+      }
       break;
     case v_front_porch + v_sync_width + v_back_porch:
-      digitalWrite(VVIS_PIN, HIGH);
+      set_pin(VVIS_PORT_NAME, VVIS_PORT_BIT);
       break;
     case whole_frame:
-      digitalWrite(VVIS_PIN, LOW);
-      PORTD &= ~_BV(PORTD2);
+      reset_pin(VVIS_PORT_NAME, VVIS_PORT_BIT);
       row_counter = 0;
       break;
   }
 }
 
 void setup() {
-  pinMode(HSYNC_PIN, OUTPUT);
-  pinMode(HVIS_PIN, OUTPUT);
-  pinMode(VSYNC_PIN, OUTPUT);
-  pinMode(VVIS_PIN, OUTPUT);
+  set_pin_output(HSYNC_PORT_NAME, HSYNC_PORT_BIT);
+  set_pin_output(VSYNC_PORT_NAME, VSYNC_PORT_BIT);
+  set_pin_output(HVIS_PORT_NAME, HVIS_PORT_BIT);
+  set_pin_output(VVIS_PORT_NAME, VVIS_PORT_BIT);
+  set_pin_output(HEARTBEAT_PORT_NAME, HEARTBEAT_PORT_BIT);
 
   // Reset all timers and halt them
   GTCCR = _BV(TSM) | _BV(PSRASY) | _BV(PSRSYNC);
@@ -158,12 +197,25 @@ void setup() {
   TCNT0 = 0;
   TCNT2 = 256-(int)(hvis_inactive_offset * timer_freq);
 
-  // Enable OVF interrupt for timer 2
-  TIMSK2 |= 0x1;
+  // Enable OVF interrupt for timer 0
+  TIMSK0 |= _BV(TOIE0);
 
-  // Start timers.
+  // Start timers and enable interrupts.
+  sei();
   GTCCR = 0;
 }
 
 void loop() {
+  set_pin(HEARTBEAT_PORT_NAME, HEARTBEAT_PORT_BIT);
+  _delay_ms(500);
+  reset_pin(HEARTBEAT_PORT_NAME, HEARTBEAT_PORT_BIT);
+  _delay_ms(500);
+}
+
+int main() {
+  setup();
+
+  while(1) { loop(); };
+
+  return 0;
 }
